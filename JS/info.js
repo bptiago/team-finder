5fccaf466400e3936fe59072e3f513da3b70f35a
Guilherme Sampaio <guisamfurquim@gmail.com>
2023-05-23 21:16:49 -0300

    atualização do dropdawn da info

--- JS/info.js
+++ JS/info.js
@@ -1,28 +1,38 @@
-function atuamain(){
+function atuamain() {
 
 }
 
-function aturank(){
+function aturank() {
 
 }
 
-function atulingua(){
+function atulingua() {
 
 }
 
-function atudia(){
+function atudia() {
 
 }
 
 function atumain() {
+
     var immain = document.getElementById("main");
     var imagemmain = document.getElementById("imgmain")
 
     if (immain.value == "") {
         imagemmain.src = "./images/valorant-logo.png"
         imagem.alt = "Logo Valorant"
+    } else if (immain.value == "sova") {
+        imagemmain.src = "./images/sova-icon.png"
+        imagem.alt = "Sova"
     } else if (immain.value == "breach") {
         imagemmain.src = "./images/breach-icon.png"
-        imagem.alt = "Logo Valorant"
+        imagem.alt = "Breach"
+    } else if (immain.value == "skye") {
+        imagemmain.src = "./images/skye-icon.png"
+        imagem.alt = ""
+    } else if (immain.value == "kayo") {
+        imagemmain.src = "./images/kayo-icon.png"
+        imagem.alt = ""
     }
 }
