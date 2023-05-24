1881aab738c05f8f64831649f50fcb1d77f76b3d
guiguitatu <guisamfurquim@gmail.com>
2023-05-24 12:13:53 -0300

    Atualização do JS da página de info

--- JS/info.js
+++ JS/info.js
@@ -1,16 +1,39 @@
-function atuamain() {
-
-}
-
 function aturank() {
 
-}
+    var valrank = document.getElementById("rank")
+    var imgrank = document.getElementById("imgrank")
 
-function atulingua() {
-
-}
-
-function atudia() {
+    if (valrank.value == ""){
+        imgrank.src = "./images/valorant-logo.png"
+        imgrank.alt = "Logo Valorant"
+    } else if (valrank.value == "ferro"){
+        imgrank.src = "./images/ferro.png"
+        imgrank.alt = "Ferro"
+    } else if (valrank.value == "bronze"){
+        imgrank.src = "./images/bronze.png"
+        imgrank.alt = "Bronze"
+    } else if (valrank.value == "prata"){
+        imgrank.src = "./images/prata.png"
+        imgrank.alt = "Prata"
+    } else if (valrank.value == "ouro"){
+        imgrank.src = "./images/ouro.png"
+        imgrank.alt = "Ouro"
+    } else if (valrank.value == "platina"){
+        imgrank.src = "./images/platina.png"
+        imgrank.alt = "Platina"
+    } else if (valrank.value == "diamante"){
+        imgrank.src = "./images/diamante.png"
+        imgrank.alt = "Diamante"
+    } else if (valrank.value == "ascendente"){
+        imgrank.src = "./images/ascendente.png"
+        imgrank.alt = "Ascendente"
+    } else if (valrank.value == "imortal"){
+        imgrank.src = "./images/imortal.png"
+        imgrank.alt = "Imortal"
+    } else if (valrank.value == "radiante"){
+        imgrank.src = "./images/radiante.png"
+        imgrank.alt = "Radiante"
+    }
 
 }
 
@@ -33,6 +56,65 @@ function atumain() {
         imagem.alt = ""
     } else if (immain.value == "kayo") {
         imagemmain.src = "./images/kayo-icon.png"
-        imagem.alt = ""
+        imagem.alt = "Kay/o"
+    } else if (immain.value == "fade") {
+        imagemmain.src = "./images/fade-icon.png"
+        imagem.alt = "Fade"
+    } else if (immain.value == "gekko") {
+        imagemmain.src = "./images/gekko-icon.png"
+        imagem.alt = "Gekko"
+    } else if (immain.value == "phoenix") {
+        imagemmain.src = "./images/phoenix-icon.png"
+        imagem.alt = "Phoenix"
+    } else if (immain.value == "reyna") {
+        imagemmain.src = "./images/reyna-icon.png"
+        imagem.alt = "Reyna"
+    } else if (immain.value == "jett") {
+        imagemmain.src = "./images/jett-icon.png"
+        imagem.alt = "Jett"
+    } else if (immain.value == "raze") {
+        imagemmain.src = "./images/raze-icon.png"
+        imagem.alt = "Raze"
+    } else if (immain.value == "yoru") {
+        imagemmain.src = "./images/yoru-icon.png"
+        imagem.alt = "Yoru"
+    } else if (immain.value == "neon") {
+        imagemmain.src = "./images/neon-icon.png"
+        imagem.alt = "Neon"
+    } else if (immain.value == "brimstone") {
+        imagemmain.src = "./images/brimstone-icon.png"
+        imagem.alt = "Brimstone"
+    } else if (immain.value == "viper") {
+        imagemmain.src = "./images/viper-icon.png"
+        imagem.alt = "Viper"
+    } else if (immain.value == "omen") {
+        imagemmain.src = "./images/omen-icon.png"
+        imagem.alt = "Omen"
+    } else if (immain.value == "astra") {
+        imagemmain.src = "./images/astra-icon.png"
+        imagem.alt = "Astra"
+    } else if (immain.value == "harbor") {
+        imagemmain.src = "./images/harbor-icon.png"
+        imagem.alt = "Harbor"
+    } else if (immain.value == "sage") {
+        imagemmain.src = "./images/sage-icon.png"
+        imagem.alt = "Sage"
+    } else if (immain.value == "cypher") {
+        imagemmain.src = "./images/cypher-icon.png"
+        imagem.alt = "Cypher"
+    } else if (immain.value == "killjoy") {
+        imagemmain.src = "./images/killjoy-icon2.png"
+        imagem.alt = "Killjoy"
+    } else if (immain.value == "chamber") {
+        imagemmain.src = "./images/chamber-icon.png"
+        imagem.alt = "Chamber"
     }
+}
+
+function atulingua() {
+
+}
+
+function atudia() {
+
 }
